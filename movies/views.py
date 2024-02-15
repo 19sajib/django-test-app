979836969539b96cbfad5bed67353c997438bbcb
Md Abu Bakkar Siddiqe Sajib <contact2sajib@gmail.com>
2024-02-15 22:49:49 +0600

    dynamic view

--- movies/views.py
+++ movies/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'movies/index.html')
+    context = {
+        'movies': ['The Batman Dark Knight', 'Gladiator', 'Prestige']
+    }
+    return render(request, 'movies/index.html', context)
 
 def about(request):
     return render(request, 'movies/about.html')
