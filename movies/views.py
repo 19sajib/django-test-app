418734eecb2e5208360ea3bac7dd1f2f8774f460
Md Abu Bakkar Siddiqe Sajib <contact2sajib@gmail.com>
2024-02-15 17:39:50 +0600

    first commit

--- movies/views.py
+++ movies/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Django...')
+    return render(request, 'movies/index.html')
+
+def about(request):
+    return render(request, 'movies/about.html')
